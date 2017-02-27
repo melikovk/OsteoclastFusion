40759d68c337e1c76356e30e1c3562607f44181c
Kamran <melikovk@gmail.com>
2017-02-26 19:41:08 -0500

    Speed up of simulate.random

--- tools.R
+++ tools.R
@@ -14,6 +14,12 @@
 #' @export
 #'
 #' @examples
+
+require(tibble)
+require(dplyr)
+require(wrswoR)
+
+
 read.excel<-function(fname) {
   require(readxl)
   require(dplyr)
@@ -96,8 +102,6 @@ fusion.indexes.mod<-function(N, nfus=round(0.3*N), B=1) {
 #' @examples
 #' 
 simulate.random<-function(dist, fnum, weights=rep(1, length(dist)), B = 1) {
-  require(tibble)
-  require(dplyr)
   out_df<-tibble(Nuclei=integer(), N=integer(), fusNum=integer())
   smax<-length(dist)
   init_dist <- dist
@@ -105,6 +109,7 @@ simulate.random<-function(dist, fnum, weights=rep(1, length(dist)), B = 1) {
   # print(c(length(dist), length(weights), fnum))
   for (k in 1:B) { 
     dist <- init_dist
+    # probs <- weights*dist
     for (i in 1:fnum) {
       c1<-sample(smax, 1, prob=weights*dist)
       dist[c1]<-dist[c1]-1
@@ -117,8 +122,48 @@ simulate.random<-function(dist, fnum, weights=rep(1, length(dist)), B = 1) {
   out_dist<-out_dist/B
   out_df<-tibble(Nuclei=1:smax, N=out_dist, fusNum=rep(fnum, smax)) %>% 
     filter(!(Nuclei>1 & N<1))
+  # print(c(smax, max(out_df$Nuclei)))
   return(out_df)
 }
+
+simulate.random_c<-compiler::cmpfun(simulate.random)
+
+simulate.random1<-function(dist, fnum, weights=identity, B = 1) {
+  out_df<-tibble(Nuclei=integer(), N=integer(), fusNum=integer())
+  init_dist <- dist
+  out_dist <- rep_len(0, length(dist))
+  # print(c(length(dist), length(weights), fnum))
+  for (k in 1:B) { 
+    dist <- init_dist
+    # probs <- weights*dist
+    for (i in 1:fnum) {
+      smax<-length(dist)
+      c1<-sample(smax, 1, prob=weights(dist))
+      dist[c1]<-dist[c1]-1
+      c2<-sample(smax, 1, prob=weights(dist))
+      dist[c2]<-dist[c2]-1
+      if (c1+c2 > smax) {
+        dist<-c(dist, rep(0,smax))
+      }
+      dist[c1+c2]<-dist[c1+c2]+1
+    }
+    dl <- length(out_dist)-length(dist)
+    if (dl>0) {
+      out_dist<-out_dist+c(dist, rep(0,dl))
+    } else {
+      out_dist<-c(out_dist, rep(0,-dl))+dist
+    }
+  }
+  out_dist<-out_dist/B
+  out_df<-tibble(Nuclei=1:length(out_dist), N=out_dist, fusNum=rep(fnum, length(out_dist))) %>% 
+    filter(!(Nuclei>1 & N<1))
+  # print(c(smax, max(out_df$Nuclei)))
+  return(out_df)
+}
+
+simulate.random1_c<-compiler::cmpfun(simulate.random1)
+
+
 
 #' Function to simulate "founder" fusion model
 #'
@@ -178,6 +223,18 @@ initial_dist <- function(data, fnum, mono_num) {
   }
   return(dist)
 }
+
+initial_dist1 <- function(data, fnum, mono_num) {
+  counts <- data %>% group_by(Nuclei) %>% summarise(N = n())
+  maxn <- max(counts$Nuclei)*2
+  dist <- c(mono_num, rep(0, maxn))
+  for (i in seq_len(nrow(counts))) {
+    dist[counts$Nuclei[i]]<-counts$N[i]
+  }
+  return(dist)
+}
+
+
 
 
 dist.to.cdf<-function(dist) {
